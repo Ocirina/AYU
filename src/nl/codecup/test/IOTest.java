5a632dcc94bdcd3f9c42681a83101a5535f91af5
Patrick van Vuuren <patr_vv@hotmail.com>
2014-10-16 12:52:29 +0200

    IOTest

---
+++ src/nl/codecup/test/IOTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package nl.codecup.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Patrick
+ *
+ */
+public class IOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
