75b6f28ef03777a541a7fd2b9c8d2db85cfc4301
Patrick van Vuuren <patr_vv@hotmail.com>
2014-10-16 13:32:47 +0200

    Added ManagerTest

---
+++ src/nl/codecup/test/ManagerTest.java
@@ -0,0 +1,22 @@
+package nl.codecup.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+import nl.codecup.src.*;
+
+public class ManagerTest {
+
+	private Manager manager;
+	
+	@Before
+	public void create() {
+		manager = new Manager();
+	}
+	
+	@Test
+	public void test() {
+
+	}
+
+}
