55057102cbd005be5afcf72b0ba7a3d760276e58
Patrick van Vuuren <patr_vv@hotmail.com>
2014-10-16 13:19:57 +0200

    IO.isMove test added

--- src/nl/codecup/test/IOTest.java
+++ src/nl/codecup/test/IOTest.java
@@ -30,7 +30,29 @@ public class IOTest {
 	
 	@Test
 	public void testIsMove() {
+		String validMoveOne = "D5-F10";
+		assertTrue(io.isMove(validMoveOne));
 		
+		String validMoveTwo = "D10-F10";
+		assertTrue(io.isMove(validMoveTwo));
+
+		String validMoveThree ="D10-F5";
+		assertTrue(io.isMove(validMoveThree));
+
+		String validMoveFour ="D5-F5";
+		assertTrue(io.isMove(validMoveFour));
+		
+		String invalidMoveOne = "D-F5";
+		assertFalse(io.isMove(invalidMoveOne));
+		
+		String invalidMoveTwo = "D-F";
+		assertFalse(io.isMove(invalidMoveTwo));
+		
+		String invalidMoveThree = "D5-F";
+		assertFalse(io.isMove(invalidMoveThree));
+		
+		String invalidMoveFour = "";
+		assertFalse(io.isMove(invalidMoveFour));
 	}
 
 }
