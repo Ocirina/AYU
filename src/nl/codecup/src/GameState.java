23ef819bb6b42c3e173d71056a197bdab88d1d99
Brian van Burken <info@creativeview.nl>
2014-11-12 12:03:43 +0100

    Removed unused imports.

--- src/nl/codecup/src/GameState.java
+++ src/nl/codecup/src/GameState.java
@@ -1,9 +1,7 @@
 package nl.codecup.src;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 public class GameState {
     private Board board;
