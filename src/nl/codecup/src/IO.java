d701c1399df28560fb0e2d13dfe2c33ee899b773
Willem van Lent <wvlent@deactro.nl>
2014-12-01 11:00:14 +0100

    Added other player check

--- src/nl/codecup/src/IO.java
+++ src/nl/codecup/src/IO.java
@@ -5,47 +5,56 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Abstract Class IO.
- * Handles the input given by the Caiaio and outputs to the Caiaio
+ * Abstract Class IO. Handles the input given by the Caiaio and outputs to the
+ * Caiaio
  */
 public abstract class IO {
-	
-	/**
-	 * Reads input from the System.in stream.
-	 * 
-	 * @return input from System.in
-	 */
-	public static String input() {
-		InputStreamReader streamReader = new InputStreamReader(System.in);
-		BufferedReader reader = new BufferedReader(streamReader);
-	    try {
-			String readLine = reader.readLine();
-			IO.debug("Read line: "+ readLine);
-			return readLine;
-		} catch (IOException e) {
-			IO.debug("IOException: "+e.getMessage());
-		}
-	    IO.debug("Returning null");
-	    return null;
-	}
-	
-	/**
-	 * Outputs the String to System.err.
-	 * 
-	 * @param output
-	 */
-	public static void output(String output) {
-		IO.debug("WAS SENT TO CAIAIO: " + output);
-		System.out.println(output);
-		System.out.flush(); 
-	}
-	
-	/**
-	 * Log
-	 * 
-	 * @param output
-	 */
-	public static void debug(String output) {
-		System.err.println(output);
-	}
+
+    private static boolean firstMessage = true;
+
+    /**
+     * Reads input from the System.in stream.
+     * 
+     * @return input from System.in
+     */
+    public static String input() {
+        InputStreamReader streamReader = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(streamReader);
+        try {
+            String readLine = reader.readLine();
+            if (!firstMessage) {
+                IO.debug("Read line: " + readLine);
+            } else {
+                firstMessage = false;
+                IO.debug("R JavaPlayer");
+            }
+
+            return readLine;
+        }
+        catch (IOException e) {
+            IO.debug("IOException: " + e.getMessage());
+        }
+        IO.debug("Returning null");
+        return null;
+    }
+
+    /**
+     * Outputs the String to System.err.
+     * 
+     * @param output
+     */
+    public static void output(String output) {
+        IO.debug("WAS SENT TO CAIAIO: " + output);
+        System.out.println(output);
+        System.out.flush();
+    }
+
+    /**
+     * Log
+     * 
+     * @param output
+     */
+    public static void debug(String output) {
+        System.err.println(output);
+    }
 }
