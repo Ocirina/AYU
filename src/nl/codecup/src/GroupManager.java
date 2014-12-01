8903e477ef8a58a0c3e306762a799aabeeb52f1f
Brian van Burken <brian@creativeview.nl>
2014-12-01 11:32:51 +0100

    GroupManager: Removed A LOT of unused code and got rid of the Singleton with DI.

--- src/nl/codecup/src/GroupManager.java
+++ src/nl/codecup/src/GroupManager.java
@@ -5,29 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GroupManager {
-    private static GroupManager instance;
-    private Group[] playerGroups;
 
-    private GroupManager() {
-    }
-
-    public void setPlayerGroups(Group[] groups) {
-        this.playerGroups = groups;
-    }
-
-    public static synchronized GroupManager getInstance() {
-        if (instance == null)
-            instance = new GroupManager();
-        return instance;
-    }
-
-    public Group[] recheckGroups(Board board) {
+    public static Group[] recheckGroups(Board board) {
         List<Group> groupsArray = new ArrayList<Group>();
         int[][] boardArray = board.getBoardContents();
 
         for (int row = 0; row < 11; row++) {
             for (int column = 0; column < 11; column++) {
-                if (boardArray[row][column] == Player.piece && this.getGroupByCoordinate(row, column, groupsArray) == null) {
+                if (boardArray[row][column] == Player.piece && getGroupByCoordinate(row, column, groupsArray) == null) {
             		Group group = new Group(groupsArray.size());
             		group.addCoordinate(row + "," + column);
             		String[] neighBors = board.getNeighbors(row, column);
@@ -35,11 +20,10 @@ public class GroupManager {
                 }
             }
         }
-        this.playerGroups = groupsArray.toArray(new Group[groupsArray.size()]);
-        return playerGroups;
+        return groupsArray.toArray(new Group[groupsArray.size()]);
     }
     
-    private Group checkNeighBorsForGroup(String[] currentNeighbors, Group group, Board board) {
+    private static Group checkNeighBorsForGroup(String[] currentNeighbors, Group group, Board board) {
     	for(String neighBor : currentNeighbors) {
     		if(!group.getCoordinates().contains(neighBor)) {
     			String[] coords = neighBor.split(",");
@@ -51,21 +35,8 @@ public class GroupManager {
     	return group;
     }
 
-    private String joinCoordinates(int x, int y) {
+    private static String joinCoordinates(int x, int y) {
         return x + "," + y;
-    }
-
-    /**
-     * Returns a group
-     * 
-     * @param x
-     *            : row
-     * @param y
-     *            : column
-     * @return The group that contains the coordinate.
-     */
-    public Group getGroupByCoordinate(int x, int y) {
-    	return getGroupByCoordinate(x, y, Arrays.asList(playerGroups));
     }
     
     /**
@@ -75,16 +46,11 @@ public class GroupManager {
      * @param groups
      * @return
      */
-    public Group getGroupByCoordinate(int x, int y, List<Group> groups) {
+    private static Group getGroupByCoordinate(int x, int y, List<Group> groups) {
         for (Group group : groups) {
             if (group != null && group.containsCoordinates(joinCoordinates(x, y)))
                 return group;
         }
         return null;
-    }
-
-    public Group getGroupByCoordinate(String coordinate) {
-        String[] coords = coordinate.split(",");
-        return this.getGroupByCoordinate(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
     }
 }
