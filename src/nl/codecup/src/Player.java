1665bc7399a7b6f740635dd74a691c1dd4821f13
Brian van Burken <brian@creativeview.nl>
2014-12-05 10:40:17 +0100

    Extracted method for generation move up in board in Player.

--- src/nl/codecup/src/Player.java
+++ src/nl/codecup/src/Player.java
@@ -100,34 +100,41 @@ public class Player {
      * @return
      */
     public Move chooseMove() {
-        int[][] content = this.state.getBoardContents();
-        int contentLength = content.length - 1;
+    	Move move = null;
+        
         this.state.recheckGroups();
         IO.debug("TRY TO FIND MOVE FOR PLAYER: " + Player.piece);
         IO.debug("AMOUNT OF GROUPS: " + state.getGroupsLength());
 
-        if (state.getGroupsLength() > 18) {
-            for (int row = 0; row < contentLength; row++) {
-                for (int column = 0; column < contentLength; column++) {
-                    if (isGapScenario(content, row, column)) {
-                        int columnPieceToMove = findLastPieceInTheRow(content, row, column, 0);
-                        return new Move(row, columnPieceToMove, row, column + 1);
-                    }
-                }
-            }
+        if (move == null && state.getGroupsLength() > 18) {
+            move = getMoveUpInBoard(this.state);
         }
 
 
-//        if (state.getGroupsLength() > 5 && state.getGroupsLength() <= 18) {
-//            Move move = getRandomMove();
-//            if (move != null) {
-//                return move;
-//            }
+//        if (move == null && state.getGroupsLength() > 5 && state.getGroupsLength() <= 18) {
+//            move = getRandomMove();
 //        }
 
-
-        return getMonteCarloMove();
+        if (move == null) {
+        	move = getMonteCarloMove(); 
+        }
+        
+        return move;
     }
+
+	private Move getMoveUpInBoard(GameState state) {
+		int[][] content = state.getBoardContents();
+        int contentLength = content.length - 1;
+		for (int row = 0; row < contentLength; row++) {
+		    for (int column = 0; column < contentLength; column++) {
+		        if (isGapScenario(content, row, column)) {
+		            int columnPieceToMove = findLastPieceInTheRow(content, row, column, 0);
+		            return new Move(row, columnPieceToMove, row, column + 1);
+		        }
+		    }
+		}
+		return null;
+	}
 
     private boolean isGapScenario(int[][] content, int row, int column) {
         return column < 9 && row < 9 && content[row][column] == Player.piece && content[row][column + 1] == Board.NONE && content[row][column + 2] == Player.piece;
