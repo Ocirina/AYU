98082a45469dfac5efa26f5af493f275d1e08501
Brian van Burken <brian@creativeview.nl>
2014-12-21 14:42:00 +0100

    New deploy script with fixes for found bugs.

--- deploy.rb
+++ deploy.rb
@@ -1,24 +1,46 @@
 @imports = []
 @content = []
+@manager = []
 
-Dir.glob("./src/nl/codecup/src/**/*").each do |file|
-  if File.file? file
-    puts file
-    File.open(file, 'r').each do |line|
-      if line.start_with? 'import'
-        @imports << line
-      elsif !line.start_with? 'package'
-        @content << line
-      end
+def handle_line(file, line)
+  if line.start_with? 'import'
+    @imports << squish(line)
+  else
+    if file.downcase.eql? './src/nl/codecup/src/manager.java'
+      @manager << clean_line(line)
+    else
+      @content << clean_line(line)
     end
   end
 end
 
-concat  = @imports.uniq.sort.join("\n")
-concat << @content.join("\n")
-concat.gsub!(/\/\*.+?\*\//m, '')
-concat.gsub!(/^\s*[\r\n]/m, '')
+def clean_line(line)
+  line = squish(line)
+  return '' if line.start_with? 'package', '/*', '*', '*/', '//', "\n"
+  line.gsub!('public', '') if line.start_with? 'public class', 'public interface', 'public abstract'
+  line
+end
 
-out_file = File.new("caiaio.java", "w")
+# http://api.rubyonrails.org/classes/String.html#method-i-squish
+def squish(str)
+  str.gsub!(/\A[[:space:]]+/, '')
+  str.gsub!(/[[:space:]]+\z/, '')
+  str.gsub!(/[[:space:]]+/, ' ')
+  str.strip
+  str
+end
+
+Dir.glob("./src/nl/codecup/src/**/*").each do |file|
+  File.open(file, 'r').each{|line| handle_line(file, line)} if File.file? file
+end
+
+@content.reject! { |c| c.empty? }
+@manager.reject! { |c| c.empty? }
+
+concat  = @imports.uniq.sort.join("\n")
+concat << 'public ' << @manager.join("\n")
+concat << @content.join("\n")
+
+out_file = File.new("Manager.java", "w")
 out_file.puts concat
 out_file.close
