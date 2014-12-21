cfc114ccbd0a4fccc2962cecbfd3548a53da6680
Brian van Burken <brian@creativeview.nl>
2014-12-21 14:52:05 +0100

    Added comments and config for main class and package namespace.

--- deploy.rb
+++ deploy.rb
@@ -1,7 +1,10 @@
 @imports = []
 @content = []
 @manager = []
+MAIN_CLASS = 'Manager'
+PACKAGE_NAMESPACE = './src/nl/codecup/src/'
 
+# This method handles the line given
 def handle_line(file, line)
   if line.start_with? 'import'
     @imports << squish(line)
@@ -10,6 +13,11 @@ def handle_line(file, line)
   end
 end
 
+# This method cleans the line given
+# - Removes whitespaces
+# - Removes comments
+# - Removes package namespace
+# - Removes public keyword from interface or class
 def clean_line(line)
   line = squish(line)
   return '' if line.start_with? 'package', '/*', '*', '*/', '//', "\n"
@@ -17,6 +25,7 @@ def clean_line(line)
   line
 end
 
+# Hard kill all whitespaces!
 # http://api.rubyonrails.org/classes/String.html#method-i-squish
 def squish(str)
   str.gsub!(/\A[[:space:]]+/, '')
@@ -26,17 +35,23 @@ def squish(str)
   str
 end
 
-Dir.glob("./src/nl/codecup/src/**/*").each do |file|
+# Loop through all files in the namespace
+Dir.glob("#{PACKAGE_NAMESPACE}**/*").each do |file|
   File.open(file, 'r').each{|line| handle_line(file, line)} if File.file? file
 end
 
+# Remove all empty lines
 @content.reject! { |c| c.empty? }
 @manager.reject! { |c| c.empty? }
 
+# First place all imports on top
 concat  = @imports.uniq.sort.join("\n")
+# Second place all content
 concat << @content.join("\n")
-concat.gsub!('class Manager', 'public class Manager')
+# Make the manager the main class
+concat.gsub!("class #{MAIN_CLASS}", "public class #{MAIN_CLASS}")
 
-out_file = File.new("Manager.java", "w")
+# Write out to a file
+out_file = File.new("#{MAIN_CLASS}.java", "w")
 out_file.puts concat
 out_file.close
